57d91c105a813e28cf7a40791f6d4ee02447c126
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2025-08-25 17:46:42 +0200

    Delay load dbgeng.dll

--- src/playground.cpp
+++ src/playground.cpp
@@ -3,9 +3,28 @@
 #include <cstdint>
 
 #include <DbgEng.h>
+#include <delayimp.h>
+
+// https://devblogs.microsoft.com/oldnewthing/20170126-00/?p=95265
+static FARPROC WINAPI delayHook(unsigned dliNotify, PDelayLoadInfo pdli)
+{
+    if (dliNotify == dliNotePreLoadLibrary)
+    {
+        if (_stricmp(pdli->szDll, "dbgeng.dll") == 0)
+        {
+            return (FARPROC)GetModuleHandleA("dbgeng.dll");
+        }
+        return (FARPROC)LoadLibraryA(pdli->szDll);
+    }
+    return 0;
+}
+
+const PfnDliHook __pfnDliNotifyHook2 = delayHook;
 
 int main(int argc, char** argv)
 {
+    // TODO: load dbgeng.dll from the right path
+    LoadLibraryA("dbgeng.dll");
     IDebugClient9* debugClient = nullptr;
     if (const auto result = DebugCreate(__uuidof(IDebugClient8), reinterpret_cast<void**>(&debugClient));
         result != S_OK)
